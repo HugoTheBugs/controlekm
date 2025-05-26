8c41e6fa19c112d2ef2b7cc3998ce4a48babf603
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2025-05-26 10:24:52 -0300

    feat: cria PersonController, Person Entidade e person.html

---
+++ src/main/java/com/hugoferreira/controlekm/controller/PersonController.java
@@ -0,0 +1,24 @@
+package com.hugoferreira.controlekm.controller;
+
+import com.hugoferreira.controlekm.entity.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/persons")
+    public String listPerson(Model model){
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person(1, "Hugo", "hugo@fema.edu.br", "18988025850"));
+        persons.add(new Person(2, "Robin", "robin@fema.edu.br", "18988025850"));
+        persons.add(new Person(3, "Batman", "batman@fema.edu.br", "18988025850"));
+        persons.add(new Person(4, "Super", "super@fema.edu.br", "18988025850"));
+        model.addAttribute("persons", persons);
+        return "person-list";
+    }
+}
